e8700d8182df50756a185780add379372885ab17
Athari Muhammad <70275655+InnovaReborn2GD@users.noreply.github.com>
2026-09-24 13:22:54 +0700

    fix: add image assets

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3ijf7rfhn7s"
+    "revision": "0.pst6nqh7kkc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
